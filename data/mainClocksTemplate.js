8b9b86d095ec140a7ad2b700fe06e00459bbf01e
Stepbackru <alex-ruba@bk.ru>
2021-08-17 23:30:13 +0300

    feat: add template for render mainClock

---
+++ data/mainClocksTemplate.js
@@ -0,0 +1,49 @@
+const mainClocksTemplate = (theme) => {
+  return [
+    {
+      el: 'div',
+      classNames: 'clock',
+      parent: 'body',
+      child: [
+        {
+          el: 'div',
+          classNames: theme ? `clock__wrapper_${theme}` : `clock__wrapper`,
+          child: [
+            {
+              el: 'div',
+              classNames: 'clock__hour',
+              child: [
+                {
+                  el: 'div',
+                  classNames: theme ? `clock__arrow-hr_${theme}` : `clock__arrow-hr`,
+                }
+              ],
+            },
+            {
+              el: 'div',
+              classNames: 'clock__min',
+              child: [
+                {
+                  el: 'div',
+                  classNames: theme ? `clock__arrow-mn_${theme}` : `clock__arrow-mn`,
+                }
+              ],
+            },
+            {
+              el: 'div',
+              classNames: 'clock__sec',
+              child: [
+                {
+                  el: 'div',
+                  classNames: theme ? `clock__arrow-sc_${theme}` : `clock__arrow-sc`,
+                }
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ];  
+}
+
+export default mainClocksTemplate;
