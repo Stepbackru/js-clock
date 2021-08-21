0250b5d95327b9901fe7ff34e8d3496ee470f8b8
Stepbackru <alex-ruba@bk.ru>
2021-08-21 23:03:08 +0300

    feat: add data of cities

---
+++ data/cities.js
@@ -0,0 +1,53 @@
+const cities = [
+  {
+    city_en: 'New York',
+    city_ru: 'Нью-Йорк',
+    country_en: 'USA',
+    country_ru: 'США',
+    utcTime: -4,
+  },
+  {
+    city_en: 'Moscow',
+    city_ru: 'Москва',
+    country_en: 'Russia',
+    country_ru: 'Россия',
+    utcTime: +3,
+  },
+  {
+    city_en: 'Male',
+    city_ru: 'Мале',
+    country_en: 'Maldives',
+    country_ru: 'Мальдивы',
+    utcTime: +5,
+  },
+  {
+    city_en: 'UTC',
+    city_ru: 'Время по Гринвичу',
+    country_en: 'England',
+    country_ru: 'Англия',
+    utcTime: +0,
+  },
+  {
+    city_en: 'Minsk',
+    city_ru: 'Минск',
+    country_en: 'Belarus',
+    country_ru: 'Беларусь',
+    utcTime: +3,
+  },
+  {
+    city_en: 'Beijing',
+    city_ru: 'Пекин',
+    country_en: 'China',
+    country_ru: 'Китай',
+    utcTime: +8,
+  },
+  {
+    city_en: 'Tokyo',
+    city_ru: 'Токио',
+    country_en: 'Japan',
+    country_ru: 'Япония',
+    utcTime: +9,
+  },
+];
+
+export default cities;
