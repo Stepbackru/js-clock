6f5f8d7a352e62fa2c7515ca07232421b0155d04
Stepbackru <alex-ruba@bk.ru>
2021-08-22 16:13:45 +0300

    feat: added constants for greating in app

---
+++ assets/utils/constants.js
@@ -0,0 +1,18 @@
+export const TIME_LANG = [
+  {
+    en: 'morning',
+    ru: 'Доброе утро'
+  },
+  {
+    en: 'day',
+    ru: 'Добрый день'
+  },
+  {
+    en: 'evening',
+    ru: 'Добрый вечер'
+  },
+  {
+    en: 'night',
+    ru: 'Доброй ночи'
+  }
+];
