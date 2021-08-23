fe1969bb0c91d4874a9dae091008a443564272fc
Stepbackru <alex-ruba@bk.ru>
2021-08-23 21:28:38 +0300

    feat: added render class setUserName to main class

--- index.js
+++ index.js
@@ -2,20 +2,32 @@ import mainClocks from './components/mainClocks.js';
 import cityBlock from './components/cityBlock.js';
 import citiesClocks from './components/citiesClocks/citiesClocks.js';
 import showDateGreating from './components/showDateGreating/showDateGreating.js';
+import setUserName from './components/setUserName/setUserName.js';
 
 class MainPage {
+  constructor() {
+    this.userNameStore = localStorage.getItem(`username`);
+  }
   
   render() {
     cityBlock.render();
     mainClocks.render();
     citiesClocks.render();
     showDateGreating.render();
+
+    if (!this.userNameStore) {
+      setUserName.render();
+    }
   }
 
   subscribe() {
     mainClocks.subscribe();
     citiesClocks.subscribe();
     showDateGreating.subscribe();
+
+    if (!this.userNameStore) {
+      setUserName.subscribe();
+    }
   }
 }
 
