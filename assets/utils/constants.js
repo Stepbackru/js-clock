3304c788aabd6c5d1076a97e97d633ceb65daa17
Stepbackru <alex-ruba@bk.ru>
2021-08-23 21:27:42 +0300

    feat: added const for text input&btn in modal window

--- assets/utils/constants.js
+++ assets/utils/constants.js
@@ -16,3 +16,22 @@ export const TIME_LANG = [
     ru: 'Доброй ночи'
   }
 ];
+
+export const TEXT_INPUT = (inputName) => {
+  const inputs = [
+    {
+      id: 'name',
+      en: 'Hello, what\'s your name?',
+      ru: 'Здравствуйте, как вас зовут?'
+    },
+  ];
+
+  return inputs.find(elem => elem.id === inputName);
+}
+
+export const BTN_SUBMIT = [
+  {
+    en: 'Submit',
+    ru: 'Отправить',
+  }
+];
