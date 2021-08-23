735189d135d47cf3b4e80d52d967dd5f52d0e48c
Stepbackru <alex-ruba@bk.ru>
2021-08-23 21:25:43 +0300

    feat: add modal window template for setUserName

---
+++ data/setUserNameTemplate.js
@@ -0,0 +1,38 @@
+import { TEXT_INPUT, BTN_SUBMIT } from "../assets/utils/constants.js";
+
+const setUserNameTemplate = () => {
+  const langStore = localStorage.getItem(`lang`);
+
+  return [
+    {
+      el: 'div',
+      classNames: 'name',
+      parent: 'body',
+      child: [
+        {
+          el: 'div',
+          classNames: 'name__wrapper',
+          child: [
+            {
+              el: 'h2',
+              classNames: 'name__caption',
+              text: `${TEXT_INPUT('name')[`${langStore}`]}`,
+            },
+            {
+              el: 'input',
+              classNames: 'name__input',
+              dataAttr: [['type', 'text'], ['autocomplete', 'off'], ['pattern','[a-zA-Zа-яА-ЯёЁ\s]+'], ['required', '']],
+            },
+            {
+              el: 'button',
+              classNames: 'name__submit',
+              text: langStore === 'en' ? `${BTN_SUBMIT[0].en}`  : `${BTN_SUBMIT[0].ru}`,
+            },
+          ],
+        },
+      ]
+    },
+  ];
+}
+
+export default setUserNameTemplate;
